a6067c0d017f5ecbf6768f81ce54e1b6224db867
Charbel Maroun <charbel-maroun@hotmail.com>
2022-11-04 18:19:05 -0400

    commit tree 597ef8f09a518b80376a4d5172c4a53488f32f8f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+
+function saturdayFun (activity){
+
+    if (activity === undefined) {
+        return "This Saturday, I want to roller-skate!"
+    } else {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+
+function mondayWork (dailyActivity){
+
+    if (dailyActivity === undefined) {
+        return "This Monday, I will go to the office."
+    } else {
+        return `This Monday, I will ${dailyActivity}.`
+    }
+}
+
+function wrapAdjective (character){
+    return function (quality = "special"){
+        return `You are ${character}${quality}${character}!`
+    }
+}
